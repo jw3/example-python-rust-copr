7c4862b2539fed525f0a300471c96a154c47d7ec
John Wass <wassj@ctc.com>
2022-08-17 17:41:28 -0400

    thiserror

---
+++ src/error.rs
@@ -0,0 +1,11 @@
+use std::io;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("File IO Error: {0}")]
+    FileIoError(#[from] io::Error),
+
+    #[error("Invalid rules detected")]
+    RuleValidationFailed,
+}
