ae34addc9f172f0930f4af4bf405e68e85d04e10
John Wass <wassj@ctc.com>
2022-08-18 20:49:50 -0400

    same dir import

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ import os
 
 from setuptools import find_namespace_packages, setup
 from setuptools_rust import RustExtension
-
-import version
+from version import get_versions
 
 
 def get_version():
     if "VERSION" in os.environ:
         return os.getenv("VERSION")
     else:
-        meta = version.get_versions()
+        meta = get_versions()
         if "version" not in meta:
             raise RuntimeError("Could not parse version from Git")
         return meta["version"]
