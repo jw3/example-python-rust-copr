88292eb805cc854666026d5b2278739ad2151993
John Wass <wassj@ctc.com>
2022-08-22 22:29:10 -0400

    select version

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import find_namespace_packages, setup
 from setuptools_rust import RustExtension
+import os
+
+
+def get_version():
+    if "VERSION" in os.environ:
+        return os.getenv("VERSION")
+    try:
+        from version import get_versions
+        meta = get_versions()
+        if "version" not in meta:
+            raise RuntimeError("Could not parse version from Git")
+        return meta["version"]
+    except Exception:
+        raise RuntimeError("Was not able to detect Python rulec version")
+
 
 setup(
     name="rulec",
-    version="0.4.3",
+    version=get_version(),
     packages=find_namespace_packages(
         include=["rulec", "rulec.*"],
     ),
